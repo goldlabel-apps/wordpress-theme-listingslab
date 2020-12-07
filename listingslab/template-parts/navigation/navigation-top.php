a890824aea921d37125d62a98024fb20c33d4d0e
Chris Dorward <listingslab@gmail.com>
2020-12-08 05:14:32 +1000

    Adds pwa button to navigation

--- listingslab/template-parts/navigation/navigation-top.php
+++ listingslab/template-parts/navigation/navigation-top.php
@@ -19,25 +19,37 @@
 		?>
 	</button>
 
-	<div class="lorem-ipsum">
-		<?php 
-	        $html = file_get_contents(get_template_directory_uri() . '/react/front/build/index.html');
-	        $html = str_replace('href="/static', 'href="'. get_template_directory_uri() .
-	            '/react/front/build/static', $html);
-	        $html = str_replace('src="/static', 'src="'. get_template_directory_uri() .
-	            '/react/front/build/static', $html);
-	        echo $html;
-		?>	
+
+	<div class="nav-grid">
+
+		<div class="nav-grid-item pwa-button">
+			<?php 
+		        $html = file_get_contents(get_template_directory_uri() . '/react/front/build/index.html');
+		        $html = str_replace('href="/static', 'href="'. get_template_directory_uri() .
+		            '/react/front/build/static', $html);
+		        $html = str_replace('src="/static', 'src="'. get_template_directory_uri() .
+		            '/react/front/build/static', $html);
+		        echo $html;
+			?>	
+		</div>
+
+		<div class="nav-grid-item grow">
+			<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'top',
+						'menu_id'        => 'top-menu',
+					)
+				);
+			?>
+		</div>
+		
 	</div>
 
-	<?php
-	wp_nav_menu(
-		array(
-			'theme_location' => 'top',
-			'menu_id'        => 'top-menu',
-		)
-	);
-	?>
+
+	
+
+	
 	<?php if ( ( listingslab_is_frontpage() || ( is_home() && is_front_page() ) ) && has_custom_header() ) : ?>
 		<a href="#content" class="menu-scroll-down"><?php echo listingslab_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'listingslab' ); ?></span></a>
 	<?php endif; ?>
